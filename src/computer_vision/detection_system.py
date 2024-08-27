8d5d448b6cceb878f3136a320cb267df7b7939c6
rorro6787 <emiliorodrigo.ecr@gmail.com>
2024-08-27 15:59:19 +0200

    Added changes to the detection system

--- src/computer_vision/detection_system.py
+++ src/computer_vision/detection_system.py
@@ -16,7 +16,11 @@ cd = os.getcwd()
 def extract_entities_image(sourcePath:str):
     original_path = os.path.dirname(sourcePath)
     results = model(sourcePath, save = True, project=original_path)
-    information = ""
+    information = "Image of dimensions: " + str(cv2.imread(sourcePath).shape[1]) + "x"+ str(cv2.imread(sourcePath).shape[0]) + "\n"
+    heat_map_path = hm.heat_map(sourcePath)
+    heat_map_array = hm.load_npy(heat_map_path)
+
+
 
     for result in results:
         boxes = result.boxes  # This contains the bounding boxes for detected objects
@@ -30,8 +34,11 @@ def extract_entities_image(sourcePath:str):
             class_id = box.cls[0].item()  # class id
             class_name = model.names[int(class_id)]  # class name
             
+            center_x = int((x1 + x2) / 2)
+            center_y = int((y1 + y2) / 2)
+
             # Print or store the results
-            information += f"{class_name} at coordinates: [{x1}, {y1}, {x2}, {y2}]\n"
+            information += f"{class_name} at coordinates: [{x1}, {y1}, {x2}, {y2}] with heat deepth of {heat_map_array[center_x][center_y]} in the centre of the image\n"
     
     carpeta = os.path.join(cd, '__pycache__')
     if os.path.isdir(carpeta):
@@ -40,4 +47,4 @@ def extract_entities_image(sourcePath:str):
     return information
 
 if __name__ == "__main__":
-    extract_entities_image("/home/rorro3382/Desktop/Universidad/4Carrera/img-desc-visually-impaired/src/computer_vision/tests/test1/download.jpeg")
+    print(extract_entities_image("/home/rorro3382/Desktop/Universidad/4Carrera/img-desc-visually-impaired/src/computer_vision/tests/test1/download.jpeg"))
