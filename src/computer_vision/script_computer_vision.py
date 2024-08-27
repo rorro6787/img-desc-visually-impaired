77c6927f8204ca4955ca94bb746f3e75030cc5ad
rorro6787 <emiliorodrigo.ecr@gmail.com>
2024-08-27 12:18:33 +0200

    Implemented functions that create the heatmap of an image

--- src/computer_vision/script_computer_vision.py
+++ src/computer_vision/script_computer_vision.py
@@ -1,1 +1,118 @@
-import ultralytics
+import cv2
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+
+def heat_map(filename: str):
+    # Download image or simply use the image path
+    # url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
+    # urllib.request.urlretrieve(url, filename)
+
+    # Configuring the model
+    model_type = "DPT_Large"      # MiDaS v3 - Large      (highest accuracy, slowest inference speed)
+    # model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+    # model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+
+    # Cargar el modelo de MiDaS
+    midas = torch.hub.load("intel-isl/MiDaS", model_type)
+
+    # Configuring device (GPU or CPU)
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    midas.to(device)
+    midas.eval()
+
+    # Download the necessary transformations
+    midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+
+    # Select the transformation according to the model type
+    if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+        transform = midas_transforms.dpt_transform
+    else:
+        transform = midas_transforms.small_transform
+
+    # Download image and convert to RGB
+    img = cv2.imread(filename)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Prepare the image for prediction  
+    input_batch = transform(img).to(device)
+
+    # Do the prediction without gradients
+    with torch.no_grad():
+        prediction = midas(input_batch)
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=img.shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+
+    # Convert the prediction to a numpy array
+    output = prediction.cpu().numpy()
+
+    original_path = os.path.dirname(filename)
+
+    # Save the depth image as an image file (heat map)
+    depth_filename = os.path.join(original_path, "depth_image.png")
+    plt.imsave(depth_filename, output, cmap='plasma')
+
+    # Save the depth array as a .npy file
+    npy_filename = os.path.join(original_path, "depth_array.npy")
+    np.save(npy_filename, output)
+
+    # return the path of the .npy
+    return npy_filename
+
+def show_heat_map(npy_filename: str):
+    # Cargar el archivo .npy
+    # npy_filename = "name.npy"
+    depth_array = np.load(npy_filename)
+    # Verificar las dimensiones del array cargado
+    print("Dimensiones del array:", depth_array.shape)
+
+    # Mostrar la imagen usando matplotlib
+    plt.imshow(depth_array, cmap='plasma')
+    plt.colorbar()
+    plt.title('Mapa de Calor de Profundidad')
+    plt.show()
+
+def load_npy(npy_filename: str):
+    # Download the .npy file
+    depth_array = np.load(npy_filename)
+    return depth_array
+
+def main():
+    if len(sys.argv) == 3 and 'i' in sys.argv:
+        if sys.argv[-1].startswith("--source="):
+            # Remove "--source=" prefix
+            sourcePath = sys.argv[-1][9:]
+            return heat_map(sourcePath)
+        else:
+            # Handle invalid argument format
+            print(f"Invalid argument format: {sys.argv[-1]}  :( Expected format: --source=sourcePath...")
+    elif len(sys.argv) == 3 and 'ii' in sys.argv:
+        if sys.argv[-1].startswith("--source="):
+            # Remove "--source=" prefix
+            sourcePath = sys.argv[-1][9:]
+            return load_npy(sourcePath)
+        else:
+            # Handle invalid argument format
+            print(f"Invalid argument format: {sys.argv[-1]}  :( Expected format: --source=sourcePath...")
+    elif len(sys.argv) == 3 and 'iii' in sys.argv:
+        if sys.argv[-1].startswith("--source="):
+            # Remove "--source=" prefix
+            sourcePath = sys.argv[-1][9:]
+            return show_heat_map(sourcePath)
+        else:
+            # Handle invalid argument format
+            print(f"Invalid argument format: {sys.argv[-1]}  :( Expected format: --source=sourcePath...")
+    else:
+        print(f"Invalid argument format: {sys.argv[-1]}  :( Expected format: --source=sourcePath...")
+
+if __name__ == '__main__':
+    main()
+    
