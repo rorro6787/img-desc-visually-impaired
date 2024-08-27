73db1c24838da3293db1bf0909ed275bdb6023e4
rorro6787 <emiliorodrigo.ecr@gmail.com>
2024-08-27 13:30:41 +0200

    Created YOLO script for entitie recognition

---
+++ src/computer_vision/YOLO.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+import sys
+from ultralytics import YOLO
+import shutil
+import random
+from zipfile import ZipFile
+import gdown
+import time
+import heat_map as hm
+
+model = YOLO("yolov8l.pt", task="detect")
+cd = os.getcwd()          
+
+def extract_entities_image(sourcePath:str):
+    original_path = os.path.dirname(sourcePath)
+    results = model(sourcePath, save = True, project=original_path)
+    information = ""
+
+    for result in results:
+        boxes = result.boxes  # This contains the bounding boxes for detected objects
+        # Indent the following code block
+        for box in boxes:
+            # Extract the bounding box coordinates
+            x1, y1, x2, y2 = box.xyxy[0].tolist()  # top-left and bottom-right corners
+            
+            # Extract other information
+            confidence = box.conf[0].item()  # confidence score
+            class_id = box.cls[0].item()  # class id
+            class_name = model.names[int(class_id)]  # class name
+            
+            # Print or store the results
+            information += f"{class_name} at coordinates: [{x1}, {y1}, {x2}, {y2}]\n"
+    
+    carpeta = os.path.join(cd, '__pycache__')
+    if os.path.isdir(carpeta):
+        shutil.rmtree(carpeta)
+        
+    return information
+
+if __name__ == "__main__":
+    extract_entities_image("/home/rorro3382/Desktop/Universidad/4Carrera/img-desc-visually-impaired/src/computer_vision/tests/test1/download.jpeg")
